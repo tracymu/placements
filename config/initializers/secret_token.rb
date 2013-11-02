130bae4988155889a5fd65164665b39c9ab7d6cb
Brenton Annan <brentonannan@brentonannan.com>
2013-11-02 19:23:34 +1100

    Base Rails 4.0.1 app w/o Turbolinks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Placements::Application.config.secret_key_base = '19a1f2360f704a27a8a119657ea3f71261674ed74f7e0707655ce6af60d1410769d7309ac53bce219b412d5ecfacb582b30814fe4778d87536c64598d6f0b70b'
