1c0ff6efe004012abc34e1b691de335bdfa30de5
Brenton Annan <brentonannan@brentonannan.com>
2013-11-03 14:35:22 +1100

    Removed DB checks from integrations specs

--- spec/integration/client_spec.rb
+++ spec/integration/client_spec.rb
@@ -12,13 +12,11 @@ describe Client do
       fill_in "Name", with: name
       click_button "Create Client"
       page.should have_content("created")
-      Client.where(name: name).count.should == 1
     end
 
     it "fails without valid name" do
       click_button "Create Client"
       page.should have_content("can't be blank")
-      Client.count.should == 0
     end
   end
 end
