ecf40d083906e092e80c244f769aea98391b30b9
tracymu <tracy.musung@moomumedia.com>
2013-11-07 17:05:04 +1100

    Can create target site, but not yet assign a site

---
+++ app/controllers/targets_controller.rb
@@ -0,0 +1,32 @@
+class TargetsController < ApplicationController
+
+	def new
+		@target = Target.new
+	end
+
+	def create
+		@target = Target.new(target_params)
+		if @target.save
+			redirect_to(target_path(@target), notice: "Target Site '#{@target.name}' Successfully Created")
+
+
+		else
+			render 'new'
+		end
+
+	end
+
+
+	def show
+
+		@target = Target.find(params[:id])
+
+	end
+
+	protected
+
+	def target_params
+		params.require(:target).permit(:name, :url)
+	end
+
+end
